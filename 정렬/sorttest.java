80e02aee2f4e88abae81b881eff3a405eb9be92c
COitpark <minhobag18@gmail.com>
2022-10-16 19:33:15 +0900

    first commit

--- 정렬/sorttest.java
+++ 정렬/sorttest.java
@@ -4,18 +4,18 @@ import java.util.Random;
 
 public class sorttest {
     public static void main(String[] args) {
-        new sorttest().solution();
+        new sorttest().solution();   
     }
     void solution(){
         int[] arr = createArray();
         arr = sortArray(arr);
         printArray(arr);
     }
-    int[] createArray(){ 
+    int[] createArray(){
         int[] arr = new int[10];
-        for(int i =0; i < arr.length;i++){
+        for(int i = 0; i < arr.length;i++){
             arr[i] = createRandomNumber();
-            for(int j = 0; j < i; j++){
+            for(int j = 0; j < i;j++){
                 if(arr[i] == arr[j]){i--;}
             }
         }
@@ -25,6 +25,15 @@ public class sorttest {
         return (int)(Math.random()*10)+1;
     }
     int[] sortArray(int[] arr){
+        for(int i = 0; i < arr.length;i++){
+            for(int j = 0; j < arr.length-1;j++){
+                if(arr[j] > arr[j+1]){
+                int t = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = t;
+                }
+            }
+        }
         return arr;
     }
     void printArray(int[] arr){
